31b832b1dfc1394cfd7f8781262ee95d0e896466
Raphael Pierquin <raphael@ut7.fr>
2018-05-10 07:54:00 +0200

    Add sample size change via config panel

--- js/main.js
+++ js/main.js
@@ -1,19 +1,28 @@
+var sampleSize = 25;
+var words;
+
 function drawScores(score1, score2) {
   document.getElementById("score1").innerText = score1;
   document.getElementById("score2").innerText = score2;
 }
 
 function levelChangeEventHandler(event) {
-    var words = {
-      cp: words_cp,
-      ce1: words_ce1,
-      ce2cm2: words_ce2cm2
-    }
-    modobble.loadWords(words[event.target.value]);
+  var words_per_level = {
+    cp: words_cp,
+    ce1: words_ce1,
+    ce2cm2: words_ce2cm2
+  }
+  words = words_per_level[event.target.value];
+  modobble.loadWords(_.take(words,sampleSize));
 }
 
 function wordsPerCardChangeEventHandler(event) {
   modobble.setNumberOfWordsPerCard(event.target.value);
+}
+
+function sampleSizeChangeEventHandler(event) {
+  sampleSize = event.target.value;
+  modobble.loadWords(_.take(words,sampleSize));
 }
 
 function toggleConfigPanel() {
@@ -21,10 +30,12 @@ function toggleConfigPanel() {
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    modobble.init(words_cp,document.getElementById("gauche"),document.getElementById("droite"),drawScores);
-    modobble.draw();
-    document.querySelector('select[name="level"]').onchange=levelChangeEventHandler;
-    document.querySelector('select[name="wordspercard"]').onchange=wordsPerCardChangeEventHandler;
-    document.getElementById('menu').onclick=toggleConfigPanel;
+  words = words_cp;
+  modobble.init(words,document.getElementById("gauche"),document.getElementById("droite"),drawScores);
+  modobble.draw();
+  document.querySelector('select[name="level"]').onchange=levelChangeEventHandler;
+  document.querySelector('select[name="wordspercard"]').onchange=wordsPerCardChangeEventHandler;
+  document.querySelector('select[name="samplesize"]').onchange=sampleSizeChangeEventHandler;
+  document.getElementById('menu').onclick=toggleConfigPanel;
 });
 
